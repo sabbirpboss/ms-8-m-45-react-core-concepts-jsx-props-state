7de35a9b62c481ac539943589de9135bf993c27d
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-16 13:15:49 +0600

    work running-m-44 continue

---
+++ my-app/src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>We're going for JavaScript React ! ! !</p>
+      <div className="myGrid">
+      <MyFirstReactProject></MyFirstReactProject>
+      <Number></Number>
+      <Number></Number>
+      <Person></Person>
+      </div>
+    </div>
+  );
+}
+
+//variable declearation
+const num1 = 222;
+const num2 = 222;
+const hablu = {name: 'Hablu Vai', job: 'Programming kore bug fix kora', age: 25}
+
+//style adding by declearing variable
+const myStyle = {
+  color : "red",
+  backgroundColor : "aqua",
+  padding : "10px 20px",
+  display: "inline-block"
+}
+
+function MyFirstReactProject() {
+  return (
+    <div className='text customStyle'>
+      <h3 style={myStyle}>1.Text testing with React JSX:</h3>
+      <h2>JSX - JavasScript XML</h2>
+      <p>XML stands for "Extensible Markup Language"</p>
+    </div>
+  )
+}
+
+function Number() {
+  return (
+    <div className='number customStyle'>
+      <h3 style={myStyle}>2.Number calculation with React JSX:</h3>
+      <h5>number function repeated</h5>
+      <p>Number: {555 + num1 + num2}</p>
+    </div>
+  )
+}
+
+function Person() {
+  return (
+    <div className='person customStyle'>
+      <h3 style={myStyle}>3.Object text testing with React JSX:</h3>
+      <p>Name: {hablu.name}</p>
+      <p>Profession: {hablu.job}</p>
+      <p>Age: {hablu.age}</p>
+    </div>
+  )
+}
+export default App;
